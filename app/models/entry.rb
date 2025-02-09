24dca8429c2b305f555aef0f64b560aa9c4e915b
Salazar <github@salazarmx.com>
2025-02-09 12:20:33 -0600

    Displaying entries'

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,16 @@
 class Entry < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  before_save :set_md5_checksum
+  
+  def content_for_preview
+    content.split("\n").first.gsub(/[#`\*_]/, '').strip
+  end
+
+  private
+
+  def set_md5_checksum
+    self.md5_checksum = Digest::MD5.hexdigest(content)
+  end
 end
