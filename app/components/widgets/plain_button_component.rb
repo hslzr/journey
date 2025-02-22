d77eae6d0b5d1a7cf8dd2169f6b9979754756e17
Salazar <github@salazarmx.com>
2025-02-22 16:50:30 -0600

    Add a new plain_button_component. For things for which a whole button feels like a lot

---
+++ app/components/widgets/plain_button_component.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class Widgets::PlainButtonComponent < ViewComponent::Base
+
+  erb_template <<-ERB
+    <button class="<%= button_classes %>">
+      <%= @text %>
+    </button>
+  ERB
+
+  def initialize(text:, variant: :base, **options)
+    @text = text
+    @variant = variant
+    @options = options
+  end
+
+  private
+
+  def button_classes
+    "
+      inline-block font-black text-black p-2 border-0 cursor-pointer bg-transparent
+      %{text_color} border-b-2 border-transparent
+      transition-all duration-300 ease-in-out
+      hover:border-black
+    ".squish % { text_color: text_color }
+  end
+
+  def text_color
+    case @variant
+    when :base
+      "text-black"
+    when :primary
+      "text-white"
+    when :secondary
+      "text-white"
+    else
+      "text-black"
+    end
+  end
+end
