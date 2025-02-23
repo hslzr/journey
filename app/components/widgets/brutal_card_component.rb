08e5c9971684b4e7bd52acbf8ca3b2357c4dc6b3
Salazar <github@salazarmx.com>
2025-02-22 23:53:25 -0600

    BrutalCard component. Might rename it

---
+++ app/components/widgets/brutal_card_component.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Widgets::BrutalCardComponent < ViewComponent::Base
+
+  erb_template <<-ERB
+    <div class="border-brutal-radius <%= shadow_bg_color %>">
+      <span class="<%= bg_color %> <%= text_color %> border-2 <%= border_color %> border-brutal-radius -translate-x-1 -translate-y-1 block px-4 py-2">
+        <%= content %>
+      </span>
+    </div>
+  ERB
+
+  def initialize(variant: :base)
+    @variant = variant
+  end
+
+  private
+
+  def shadow_bg_color
+    case @variant
+    when :base
+      "bg-black"
+    when :danger
+      "bg-red-500"
+    end
+  end
+
+  def border_color
+    case @variant
+    when :base
+      "border-black"
+    when :danger
+      "border-red-500"
+    end
+  end
+
+  def bg_color
+    case @variant
+    when :base
+      "bg-white"
+    when :danger
+      "bg-red-50"
+    end
+  end
+
+  def text_color
+    case @variant
+    when :base
+      "text-black"
+    when :danger
+      "text-red-500"
+    end
+  end
+end
---
+++ test/components/widgets/brutal_card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Widgets::BrutalCardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Widgets::BrutalCardComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
